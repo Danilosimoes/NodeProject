32b4522a40561244c891a49be90b363593970de5
danilossimoes <danilossimoes28@gmail.com>
2021-12-02 08:53:20 -0300

    alter server port to 8080

--- VidroLaser/server.js
+++ VidroLaser/server.js
@@ -1,7 +1,7 @@
 const http = require('http');
 const { userInfo } = require('os');
 const app = require('./app');
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 8080;
 
 
 
