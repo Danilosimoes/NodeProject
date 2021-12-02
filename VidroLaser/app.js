7d6c64e7bee881f40a176f3a9193454edea3457e
danilossimoes <danilossimoes28@gmail.com>
2021-12-02 17:17:27 -0300

    trying

--- VidroLaser/app.js
+++ VidroLaser/app.js
@@ -14,8 +14,8 @@ app.use(bodyParser.urlencoded({extended: true})); //apenas dados simples
 app.use(bodyParser.json()); //json de entrada no body
 app.use(express.json());
 
-app.use('*',(req, res, next) => {
-    res.header('Acess-Control-Allow-Origin', '*');
+app.use((req, res, next) => {
+    res.setHeader('Acess-Control-Allow-Origin', '*');
     res.setHeader(
         'Acess-Control-Allow-Header',
         'Origin',
@@ -28,21 +28,18 @@ app.use('*',(req, res, next) => {
     res.setHeader("Access-Control-Allow-Methods", "PUT", "POST, GET, DELETE");
     res.setHeader("Access-Control-Max-Age", "3600");
     res.setHeader("Access-Control-Allow-Headers", "x-requested-with", "content-type");
-    res.setHeader('Acess-Control-Allow-Methods', 'PUT, POST, PATCH, DELETE, GET');
-    
+
 
     
 
-    res.setHeader("Access-Control-Allow-Origin", "*");
-    res.setHeader("Access-Control-Allow-Credentials", "true");
-    res.setHeader("Access-Control-Allow-Methods", "GET,HEAD,POST,PUT, DELETE");
-    res.setHeader("Access-Control-Allow-Headers", "Access-Control-Allow-Headers", "Origin,Accept","Authorization", "X-Requested-With", "Content-Type", "Access-Control-Request-Method", "Access-Control-Request-Headers");
+    
     app.use(cors());   
    
-    //if(req.method === 'OPTIONS'){
+    if(req.method === 'OPTIONS'){
+        res.setHeader("Access-Control-Allow-Methods", "PUT", "POST, GET, DELETE");
         //return res.status(200).send({});
         
-    //}
+    }
         
 
 
