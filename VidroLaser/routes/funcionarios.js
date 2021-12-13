dcff31d3c04051cd2a5d3167b82fc72d09b35710
danilossimoes <danilossimoes28@gmail.com>
2021-12-13 11:45:15 -0300

    est

--- VidroLaser/routes/funcionarios.js
+++ VidroLaser/routes/funcionarios.js
@@ -6,7 +6,7 @@ const login = require('../middleware/login');
 const funcController = require('../controllers/funcionarios-controller');
 
 
-router.get('/findalAllFuncionario', login.opcional, funcController.getAllFunc );
+router.get('/findFuncionarios', login.opcional, funcController.getAllFunc );
 
 router.post('/cadastrarFuncionario', /*login.obrigatorio,*/ funcController.postFuncionarios);
 
